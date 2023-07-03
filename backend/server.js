4e344e3c75e4e2f55de228103119e5ec4e91adc5
AbdulHadiImran <hadiimran2k@vt.edu>
2023-07-03 03:10:39 -0400

    Mern app

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+// creates an express app and stores it
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next)=> {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+
+            // listen for requests
+        app.listen(process.env.PORT , () => {
+        console.log('connected to DB and listening on port 4000!!!')
+        })
+
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
+
+
