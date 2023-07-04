2b67cbca6b47f4c6e135e26725506af59712f324
AbdulHadiImran <hadiimran2k@vt.edu>
2023-07-04 10:21:53 -0400

    Signup & Login forms

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,9 +4,15 @@ const Navbar = () =>{
     return (
         <header>
             <div className="container">
-                <Link>
+                <Link to= "/">
                     <h1> Workout Buddy </h1>
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
+                </nav>
             </div>
         </header>
     )
